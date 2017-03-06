2412d79bfa117472bfa876b28343c429809062a0
Bin Li <lb583119533@outlook.com>
2017-03-06 15:32:13 +1300

    update

--- Programming Practice/src/sixProgram/Program1.java
+++ Programming Practice/src/sixProgram/Program1.java
@@ -2,34 +2,35 @@ package sixProgram;
 
 public class Program1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int count = 0;// stop condition for the function chain
-		function1(count);
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int count = 0;
+        int limitation = 15; // stop condition
+        function1(count, limitation);
+    }
 
-	public static void function1(int count) {
-		System.out.println("The function1 fired!" + count);
+    public static void function1(int count, int limitation) {
+        System.out.println("The function1 fired! count = " + count);
 
-		if (count <= 15)
-			function2(++count);
-	}
+        if (count <= limitation)
+            function2(++count,limitation);
+    }
 
-	public static void function2(int count) {
-		System.out.println("The function2 fired!" + count);
-		if (count <= 15)
-			function3(++count);
-	}
+    public static void function2(int count, int limitation) {
+        System.out.println("The function2 fired! count = " + count);
+        if (count <= limitation)
+            function3(++count,limitation);
+    }
 
-	public static void function3(int count) {
-		System.out.println("The function3 fired!" + count);
-		if (count <= 15)
-			function4(++count);
-	}
+    public static void function3(int count, int limitation) {
+        System.out.println("The function3 fired! count = " + count);
+        if (count <= limitation)
+            function4(++count,limitation);
+    }
 
-	public static void function4(int count) {
-		System.out.println("The function4 fired!" + count);
-		if (count <= 15)
-			function1(++count);
-	}
+    public static void function4(int count, int limitation) {
+        System.out.println("The function4 fired! count = " + count);
+        if (count <= limitation)
+            function1(++count,limitation);
+    }
 }
